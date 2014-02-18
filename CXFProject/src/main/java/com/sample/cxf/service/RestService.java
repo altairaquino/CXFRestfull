aeaca95b89df47c879f5a547d5a4b8ee13b7949d
Altair Aquino <altairaquino@gmail.com>
2014-02-18 19:18:39 -0200

    Update RestService.java

--- CXFProject/src/main/java/com/sample/cxf/service/RestService.java
+++ CXFProject/src/main/java/com/sample/cxf/service/RestService.java
@@ -11,8 +11,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.springframework.stereotype.Service;
+
 import com.sample.cxf.dto.JsonResponse;
 
+@Service
 @Path("/example")
 public class RestService {
 	
@@ -23,6 +26,24 @@ public class RestService {
 		List<JsonResponse> list = new ArrayList<JsonResponse>();
 		try {
 			list.add(new JsonResponse(name));
+			return Response.ok(list).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(Status.METHOD_NOT_ALLOWED).build();
+		}
+	}
+	
+	@GET
+	@Path("/test2{name:(/name/[^/]+?)?}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response test2(@PathParam("name") String name) {
+		List<JsonResponse> list = new ArrayList<JsonResponse>();
+		try {
+			if (name == null){
+				list.add(new JsonResponse("vazio"));			
+			}else{				
+				list.add(new JsonResponse(name));
+			}
 			return Response.ok(list).build();
 		} catch (Exception e) {
 			e.printStackTrace();
